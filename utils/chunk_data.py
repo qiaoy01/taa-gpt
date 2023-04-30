fce49fd7c7777961bdc676975f0f53fa6dfd4e2c
Chatty Fish <chattyu@outlook.com>
2023-04-30 09:54:07 -0400

    add append mode

--- utils/chunk_data.py
+++ utils/chunk_data.py
@@ -3,6 +3,7 @@ from transformers import BertTokenizer
 from datasets import load_dataset
 import os
 import tqdm
+import json
 
 def tokenize_and_chunk_data(data, tokenizer):
     tokenized_data = tokenizer.encode_plus(data, padding=True, truncation=True, return_tensors='pt')
@@ -20,13 +21,20 @@ def tokenize_and_chunk_data(data, tokenizer):
 
     return chunks
 
-def convert_and_save_data(dataset, output_path, tokenizer):
-    # Initialize the binary file
-    with open(output_path, 'wb') as f:
-        pass
+def convert_and_save_data(dataset, output_path, tokenizer, mode, json_path):
+    # Load or initialize the JSON database
+    if mode == 'append':
+        with open(json_path, 'r') as f:
+            processing_state = json.load(f)
+        start_idx = processing_state['last_processed_idx'] + 1
+    else:  # init mode
+        start_idx = 0
+        processing_state = {'last_processed_idx': -1}
+        with open(json_path, 'w') as f:
+            json.dump(processing_state, f)
 
     # Iterate through the dataset with a progress bar
-    for idx, data in enumerate(tqdm.tqdm(dataset, desc="Processing dataset")):
+    for idx, data in enumerate(tqdm.tqdm(dataset[start_idx:], desc="Processing dataset", initial=start_idx, total=len(dataset))):
         # Tokenize and chunk the data
         chunks = tokenize_and_chunk_data(data['text'], tokenizer)
 
@@ -39,9 +47,11 @@ def convert_and_save_data(dataset, output_path, tokenizer):
                 chunk_data = chunk[:, 1:2049].cpu().numpy().tobytes()
                 f.write(chunk_data)
 
-        # Print progress
-        if (idx + 1) % 1000 == 0:
-            print(f"Processed {idx + 1} samples")
+        # Save the processing state every 1000 iterations
+        if (idx + start_idx) % 1000 == 0:
+            processing_state['last_processed_idx'] = idx + start_idx
+            with open(json_path, 'w') as f:
+                json.dump(processing_state, f)
 
 # Load the OpenWebText dataset
 openwebtext = load_dataset('openwebtext')
@@ -49,9 +59,13 @@ openwebtext = load_dataset('openwebtext')
 # Set the tokenizer name and output path
 tokenizer_name = 'bert-base-uncased'
 output_path = 'output.bin'
+json_path = 'processing_state.json'
 
 # Load the tokenizer
 tokenizer = BertTokenizer.from_pretrained(tokenizer_name)
 
+# Set the mode: 'init' or 'append'
+mode = 'init'
+
 # Convert and save the dataset
-convert_and_save_data(openwebtext['train'], output_path, tokenizer)
+convert_and_save_data(openwebtext['train'], output_path, tokenizer, mode, json_path)
